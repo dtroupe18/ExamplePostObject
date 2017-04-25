0e18f52735e61bb93e5f6685406cc416dc140151
Dave <Dave.Troupe@gmail.com>
2017-04-25 13:09:30 -0400

    Added Second Class

---
+++ src/AnotherOne.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Dave on 4/25/17.
+ */
+public class AnotherOne {
+    public static void main(String[] args) {
+        Post secondPost = new Post("CS1 is Great");
+        secondPost.setComment("Comment");
+        System.out.println(secondPost.toString());
+    }
+}
